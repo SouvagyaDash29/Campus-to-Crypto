bc85145092105e170cdacfb0d17cc7335f030b33
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-02-27 19:40:17 +0530

    add product page and css file

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { useTypewriter } from "react-simple-typewriter";
+import planet from "../../Assets/svg/81.svg";
+import planet2 from "../../Assets/svg/82.svg";
 import "./Product.css";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter, FaYoutube } from "react-icons/fa6";
 
 const Product = () => {
-  const [typeEffect] = useTypewriter({
-    words: ["...", "...", "..."],
-    loop: true,
-    typeSpeed: 100,
-    deleteSpeed: 40,
-  });
   return (
-    <div className="Product">
-      Coming Soon <span>{typeEffect}</span>
-      {/* <Cursor style={{ color: "white" }} /> */}
+    <div className="product-section">
+      <div className="product-circle1"></div>
+      <div className="product-circle2"></div>
+      <div className="product-circle3"></div>
+      <img src={planet} alt="icon" />
+      <div className="wrapper">
+        <svg>
+          <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+            COMMING SOON
+          </text>
+        </svg>
+      </div>
+      <div className="Product-Social-media-Section">
+        <FaFacebook />
+        <FaInstagram />
+        <FaLinkedin />
+        <FaXTwitter />
+        <FaYoutube />
+      </div>
+      <div className="product-section-3">
+        <div className="planet1"><img src={planet} alt="icon" /></div>
+        <div className="product-discussionSection">
+          <input type="text" placeholder="Write your thoughts" />
+          <button>Subscribe</button>
+        </div>
+        <div className="planet2"><img src={planet2} alt="icon" /></div>
+      </div>
     </div>
   );
 };
